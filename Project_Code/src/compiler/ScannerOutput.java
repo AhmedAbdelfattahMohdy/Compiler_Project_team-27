423134f31de6f9e23e9e34926c0ccacdee7daa6f
AhmedAbdelfattahMohdy <66708650+AhmedAbdelfattahMohdy@users.noreply.github.com>
2021-06-04 21:44:21 +0200

    Add files via upload

---
+++ Project_Code/src/compiler/ScannerOutput.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package compiler;
+
+/**
+ *
+ * @author Link
+ */
+public class ScannerOutput {
+    public int LineNo;
+    public String Lexeme;
+    public String ReturnToken;
+    public int LexemeNOinLine;
+    public boolean matchability;
+
+    public ScannerOutput(int LineNo, String Lexeme, String ReturnToken, int LexemeNOinLine, boolean matchability) {
+        this.LineNo = LineNo;
+        this.Lexeme = Lexeme;
+        this.ReturnToken = ReturnToken;
+        this.LexemeNOinLine = LexemeNOinLine;
+        this.matchability = matchability;
+    }
+    
+    
+}
